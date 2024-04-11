81dc0a4e7f2747b9c4b223bfe184c162b1ec18a4
tommitoan <htm1995@gmail.com>
2024-04-11 22:30:57 +0700

    setup testing database

---
+++ pkg/repository/dbrepo/users_postgres_test.go
@@ -0,0 +1,33 @@
+package dbrepo
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/ory/dockertest/v3"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "postgres"
+	dbName   = "users_test"
+	port     = "5435"
+	dns      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
+)
+
+var resource *dockertest.Resource
+var pool *dockertest.Pool
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+
+
+	code := m.Run()
+
+	os.Exit(code)
+}
