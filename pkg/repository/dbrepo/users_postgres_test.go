e8aa7f6dc6a5e7a5b28f961ec659c436974f9200
tommitoan <htm1995@gmail.com>
2024-04-15 22:52:56 +0700

    testing insert user

--- pkg/repository/dbrepo/users_postgres_test.go
+++ pkg/repository/dbrepo/users_postgres_test.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
+	"webapp/pkg/data"
+	"webapp/pkg/repository"
 
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
@@ -26,6 +29,7 @@ var (
 var resource *dockertest.Resource
 var pool *dockertest.Pool
 var testDB *sql.DB
+var testRepo repository.DatabaseRepo
 
 func TestMain(m *testing.M) {
 	// connect to docker; fail if docker not running
@@ -39,7 +43,7 @@ func TestMain(m *testing.M) {
 	// set up our docker options, specifying the image and so forth
 	opts := dockertest.RunOptions{
 		Repository: "postgres",
-		Tag: "14.5",
+		Tag:        "14.5",
 		Env: []string{
 			"POSTGRES_USER=" + user,
 			"POSTGRES_PASSWORD=" + password,
@@ -80,6 +84,8 @@ func TestMain(m *testing.M) {
 		log.Fatalf("error creating tables: %s", err)
 	}
 
+	testRepo = &PostgresDBRepo{DB: testDB}
+
 	// run tests
 	code := m.Run()
 
@@ -113,3 +119,25 @@ func Test_pingDB(t *testing.T) {
 		t.Error("can't ping database")
 	}
 }
+
+func TestPostgresDBRepoInsertUser(t *testing.T) {
+	testUser := data.User{
+		FirstName: "Admin",
+		LastName: "User",
+		Email: "admin@example.com",
+		Password: "secret",
+		IsAdmin: 1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	id, err := testRepo.InsertUser(testUser)
+	if err != nil {
+		t.Errorf("insert user returned an error: %s", err)
+	}
+
+	if id != 1 {
+		t.Errorf("insert user returned wrong id; expected 1, but got %d", id)
+	}
+}
+
