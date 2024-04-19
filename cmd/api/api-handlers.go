b091cc4b48cc60b02fc54e4f8795f1dab5c8c579
tommitoan <htm1995@gmail.com>
2024-04-19 23:29:19 +0700

    Test refresh token

--- cmd/api/api-handlers.go
+++ cmd/api/api-handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"errors"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -48,7 +51,61 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
-	
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	refreshToken := r.Form.Get("refresh_token")
+	claims := &Claims{}
+
+	_, err = jwt.ParseWithClaims(refreshToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(app.JWTSecret), nil
+	})
+
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	if time.Unix(claims.ExpiresAt.Unix(), 0).Sub(time.Now()) > 30 * time.Second {
+		app.errorJSON(w, errors.New("refresh token does not need renewed yet"), http.StatusTooEarly)
+		return
+	}
+
+	// get the user id from the claims
+	userID, err := strconv.Atoi(claims.Subject)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	user, err := app.DB.GetUser(userID)
+	if err != nil {
+		app.errorJSON(w, errors.New("unknown user"), http.StatusBadRequest)
+		return
+	}
+
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name: "__Host-refresh_token",
+		Path: "/",
+		Value: tokenPairs.RefreshToken,
+		Expires: time.Now().Add(refreshTokenExpiry),
+		MaxAge: int(refreshTokenExpiry.Seconds()),
+		SameSite: http.SameSiteStrictMode,
+		Domain: "localhost",
+		HttpOnly: true,
+		Secure: true,
+	})
+
+	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
 
 func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {
--- cmd/api/api-handlers_test.go
+++ cmd/api/api-handlers_test.go
@@ -4,14 +4,17 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
+	"time"
+	"webapp/pkg/data"
 )
 
 func Test_app_authenticate(t *testing.T) {
-	var theTests = []struct{
-		name string
-		requestBody string
+	var theTests = []struct {
+		name               string
+		requestBody        string
 		expectedStatusCode int
 	}{
 		{"valid user", `{"email":"admin@example.com","password":"secret"}`, http.StatusOK},
@@ -35,4 +38,58 @@ func Test_app_authenticate(t *testing.T) {
 			t.Errorf("%s: returned wrong status code; expected %d but got %d", e.name, e.expectedStatusCode, rr.Code)
 		}
 	}
+}
+
+func Test_app_refresh(t *testing.T) {
+	var tests = []struct {
+		name               string
+		token              string
+		expectedStatusCode int
+		resetRefreshTime   bool
+	}{
+		{"valid", "", http.StatusOK, true},
+		{"valid but not yet ready to expire", "", http.StatusTooEarly, false},
+		{"expired token", expiredToken, http.StatusBadRequest, false},
+	}
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+
+	oldRefreshTime := refreshTokenExpiry
+
+	for _, e := range tests {
+		var tkn string
+		if e.token == "" {
+			if e.resetRefreshTime {
+				refreshTokenExpiry = time.Second * 1
+			}
+			tokens, _ := app.generateTokenPair(&testUser)
+			tkn = tokens.RefreshToken
+		} else {
+			tkn = e.token
+		}
+
+		postedData := url.Values{
+			"refresh_token": {tkn},
+		}
+
+		req, _ := http.NewRequest("POST", "/refresh-token", strings.NewReader(postedData.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(app.refresh)
+		handler.ServeHTTP(rr, req)
+
+		if rr.Code != e.expectedStatusCode {
+			t.Errorf("%s: expected status of %d but got %d", e.name, e.expectedStatusCode, rr.Code)
+		}
+
+		refreshTokenExpiry = oldRefreshTime
+	}
+
+	
 }
